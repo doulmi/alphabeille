e93178f5ef66f6890f5eb11f4227f08e3b029116
CHEN Fengyu <doulmi@126.com>
2016-06-14 02:35:10 +0200

    cloud and sugguest bug

--- resources/views/lessons/show.blade.php
+++ resources/views/lessons/show.blade.php
@@ -84,7 +84,63 @@
         </div>
 
 {{--        @include('sugguest')--}}
-        @include('utils.topicSugguest')
+        <div class="Header"></div>
+        <h2 class="Heading-Fancy row">
+            <span class='title'>{{ trans('labels.suggestTopics')}}</span>
+        </h2>
+
+        <div class="Card-Collection">
+            <div class="row">
+                @foreach($topics as $topic)
+                    <div class="col-md-3 col-xs-6 col-sm-4">
+                        <div class="Card">
+                    <span class="Card-difficulty">
+                        {{ trans('labels.' . $topic->level) }}
+                    </span>
+
+                            @if($topic->isNew)
+                                <span class="Card-new-status Label Label-x-small">
+                        {{trans('labels.new')}}
+                    </span>
+                            @elseif($topic->isUpdated)
+                                <span class="Card-updated-status Label Label-x-small">
+                        {{ trans('labels.updated') }}
+                    </span>
+                            @endif
+                            <div class="Card-image">
+                                <a href="{{ url('topics/' . $topic->id) }}">
+                                    <img src="{{$topic->avatar}}" class="Card-image" alt="{{$topic->title}}">
+                                    <div class="Card-overlay">
+                                        <i class="glyphicon glyphicon-play-circle"></i>
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="Card-details">
+                                <h3 class="Card-title">
+                                    <a href="{{ url('/topics/' . $topic->id) }}">{{$topic->title}}</a>
+                                </h3>
+                                <div class="Card-count">{{ $topic->lessonCount() }} <span
+                                            class="utility-muted"> {{trans('labels.lessons')}}</span>
+                                </div>
+                            </div>
+                            <div class="Card-footer">
+                                <div class="hidden-xs Card-footer-content">
+                        <span class="topic-view">
+                            <span class="glyphicon glyphicon-eye-open"><span
+                                        class="g-font">{{ $topic->views() }} </span></span>
+                        </span>
+
+                        <span class="topic-like">
+                            <span class="glyphicon glyphicon-heart"><span
+                                        class="g-font">{{ $topic->likes() }} </span></span>
+                        </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
         <div class="Header"></div>
     </div>
 
