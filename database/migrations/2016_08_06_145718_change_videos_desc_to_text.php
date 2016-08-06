1fab63f05c3c7423ae2cfee141e10208b3581514
doulmi <doulmi@126.com>
2016-08-06 17:01:10 +0200

    desc to text

---
+++ database/migrations/2016_08_06_145718_change_videos_desc_to_text.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeVideosDescToText extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('videos', function (Blueprint $table) {
+            $table->text('desc')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    }
+}
