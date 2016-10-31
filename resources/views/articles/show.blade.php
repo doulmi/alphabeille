3ec10c6209ed69931c6020728d21d06d89669334
doulmi <doulmi@126.com>
2016-10-31 23:28:18 +0100

    day90 -3

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -26,6 +26,7 @@
 <div class="container ">
     <h1 class="center">90天法语背诵群</h1>
     <h3 class="center">{{$article->reciteAt}}</h3>
+    <h3 class="center">Alphabeille蜂言法语</h3>
     <center><img src="{{asset('img/168x168.jpg')}}" alt="icon" class="icon"></center>
     <div class="share-component share-panel hidden-xs hidden-md hidden-sm" data-sites="wechat"
          data-description="@lang('labels.shareTo')" >
